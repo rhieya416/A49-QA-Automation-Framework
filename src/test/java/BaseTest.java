bd49a55a2d5d903acf153c0c45392f3265a19282
rhieya416 <maria.ogden@testpro.io>
2023-09-06 12:20:05 -0700

    Homework22
    POM

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,5 +1,8 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeSuite;
 
 public class BaseTest {
@@ -8,4 +11,8 @@ public class BaseTest {
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
     }
+    public static WebDriver driver = null;
+    public static String url = null;
+    public static WebDriverWait wait = null;
+    public static Actions actions = null;
 }
